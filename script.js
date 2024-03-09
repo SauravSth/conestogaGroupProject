9e2913d36ec834a4c8b397905cfdfba56c09364d
Saurav Shrestha <sauravshrestha28@gmail.com>
2024-03-09 13:42:00 -0500

    design changes

--- script.js
+++ script.js
@@ -23,6 +23,23 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const getRandomColor = () => {
-	let randomColor = Math.floor(Math.random() * 16777215).toString(16)
-	return randomColor
+	let hexColor = Math.floor(Math.random() * 16777215).toString(16)
+	const newShade = (hexColor, magnitude = 50) => {
+		darkerColor = hexColor.replace(`#`, ``)
+		if (darkerColor.length === 6) {
+			const decimalColor = parseInt(darkerColor, 16)
+			let r = (decimalColor >> 16) + magnitude
+			r > 255 && (r = 255)
+			r < 0 && (r = 0)
+			let g = (decimalColor & 0x0000ff) + magnitude
+			g > 255 && (g = 255)
+			g < 0 && (g = 0)
+			let b = ((decimalColor >> 8) & 0x00ff) + magnitude
+			b > 255 && (b = 255)
+			b < 0 && (b = 0)
+		} else {
+			return darkerColor, hexColor
+		}
+	}
+	return `#${(g | (b << 8) | (r << 16)).toString(16)}`, hexColor
 }
