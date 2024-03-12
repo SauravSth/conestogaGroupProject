552bf4d583158799292971d1d8838d157c0f9bd5
Saurav Shrestha <sauravshrestha28@gmail.com>
2024-03-12 09:50:31 -0400

    logo and style updates + footer

--- js/getAll.js
+++ js/getAll.js
@@ -71,27 +71,27 @@ function displayTasks(tasks = TaskManager.getTasks().reverse()) {
 }
 
 function deleteTask(event, taskId) {
-	event.preventDefault();
-	TaskManager.deleteTask(taskId);
-	displayTasks();
+	event.preventDefault()
+	TaskManager.deleteTask(taskId)
+	displayTasks()
 }
 
 function editTask(event, taskId) {
-    event.preventDefault();
-    // Retrieve task data from local storage
-    const task = TaskManager.findTaskById(taskId);
-    if (task) {
-        // Encode task data as URL parameters
-        const queryParams = new URLSearchParams();
-        queryParams.set('taskId', taskId);
-        queryParams.set('taskName', task.taskName);
-        queryParams.set('taskDesc', task.taskDesc);
-        queryParams.set('assignedTo', task.assignedTo);
-        queryParams.set('deadline', task.deadline);
-        queryParams.set('priority', task.priority);
-        // Redirect to index.html with task data as URL parameters
-        window.location.href = `index.html?${queryParams.toString()}`;
-    }
+	event.preventDefault()
+	// Retrieve task data from local storage
+	const task = TaskManager.findTaskById(taskId)
+	if (task) {
+		// Encode task data as URL parameters
+		const queryParams = new URLSearchParams()
+		queryParams.set('taskId', taskId)
+		queryParams.set('taskName', task.taskName)
+		queryParams.set('taskDesc', task.taskDesc)
+		queryParams.set('assignedTo', task.assignedTo)
+		queryParams.set('deadline', task.deadline)
+		queryParams.set('priority', task.priority)
+		// Redirect to index.html with task data as URL parameters
+		window.location.href = `index.html?${queryParams.toString()}`
+	}
 }
 
 function searchTasks(event) {
